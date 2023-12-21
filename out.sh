35fd833f2a49f541f791aae3ed0c3c9de97ccbe8
Dylan Barratt <dylanbarratt03@gmail.com>
2023-12-21 13:38:24 +0000

    fixed testing script

--- out.sh
+++ out.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-java -jar /usr/local/lib/antlr-4.13.1-complete.jar LexiTrain.g4
-javac LexiTrain*.java
+java -jar /usr/local/lib/antlr-4.13.1-complete.jar LexiTrain.g4 -o ./Java
+ls ./Java/*.java
+echo "Java files generated"
+javac ./Java/LexiTrain*.java
+ls ./Java/
+echo "Java files compiled"
 
-ls *.java
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig LexiTrain lexi -gui ./examples/test.lt
+cd Java
+cp ../*.g4 ./
+java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig LexiTrain lexi -gui ../examples/test.lt
+rm *.g4
