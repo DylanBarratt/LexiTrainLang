c991008f8741293869211e88ca94b0bba3eab7f7
DylanBarratt <dylanbarratt03@gmail.com>
2023-12-26 20:55:29 +0000

    added loop examples

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ./out.sh
 
+TEST_FILE="../examples/metadata.lt"
+
 cd Java
 cp ../*.g4 ./
-java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig LexiTrain lexi -tokens ../examples/test2.lt
-java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig LexiTrain lexi -gui ../examples/test2.lt
+java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig LexiTrain lexi -tokens $TEST_FILE
+java -Xmx500M -cp "/usr/local/lib/antlr-4.13.1-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig LexiTrain lexi -gui $TEST_FILE
 rm *.g4
