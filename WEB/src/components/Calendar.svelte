1ae5e36e99f95e5979117aab500ab172fff159be
DylanBarratt <dylanbarratt03@gmail.com>
2024-03-23 10:18:24 +0000

    added date indent + outdent properly

--- WEB/src/components/Calendar.svelte
+++ WEB/src/components/Calendar.svelte
@@ -4,6 +4,7 @@ export let calendarData : any;
 
 let currentDate : Date = new Date();
 let dateIndent = getDateIndent();
+let dateOutdent = getDateOutdent();
 
 let daysInMonth = Array.from({ 
   length: getDaysInMonth(currentDate.getFullYear(), currentDate.getMonth()) 
@@ -15,24 +16,11 @@ function setDaysInMonth() {
   }, (_, i) => i + 1);
 }
 
-function getFirstDayOfMonth(year, month) {
-  // Create a new Date object with the given year and month (months are zero-indexed)
-  const firstDayOfMonth = new Date(year, month, 1);
-  
-  // Use the getDay() method to get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
-  const dayOfWeek = firstDayOfMonth.getDay();
-
-  // Define an array of day names
-  const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-  // Return the name of the first day of the month
-  return dayNames[dayOfWeek];
-}
-
 function resetDate() {
   currentDate = new Date(currentDate);
   setDaysInMonth();
   dateIndent = getDateIndent();
+  dateOutdent = getDateOutdent();
 }
 
 function prevMonth() {
@@ -66,79 +54,116 @@ function filterDataByDate(targetDate: Date) {
   });
 }
 
-function getDateIndent() {
+function getFirstDayOfMonth(): Date {
   const y = currentDate.getFullYear(); // Get the current year
   const m = currentDate.getMonth(); // Get the current month (0-indexed)
 
   // Create a new Date object representing the first day of the month
   const firstDayOfMonth = new Date(y, m, 1);
+  return firstDayOfMonth;
+}
 
-  //convert to me days
+function getLastDayOfMonth(): Date {
+  const y = currentDate.getFullYear(); // Get the current year
+  const m = currentDate.getMonth(); // Get the current month (0-indexed)
+
+  // Create a new Date object for the next month's first day
+  const nextMonthFirstDay = new Date(y, m + 1, 1);
+  
+  // Subtract one day from the next month's first day
+  const lastDayOfMonth = new Date(nextMonthFirstDay.getTime() - 1);
+
+  return lastDayOfMonth;
+}
+
+function getNextDay(currDate: Date, previousAmnt: number): Date {
+  let previousDate = new Date(currDate);
+  previousDate.setDate(previousDate.getDate() + previousAmnt); 
+  return previousDate;
+}
+
+function getDateIndent(): number {
+  const firstDayOfMonth = getFirstDayOfMonth();
+
   if (firstDayOfMonth.getDay() == 0) {
-    return 7;
-  } 
+    return 6;
+  }
 
-  // Get the day of the week for the first day of the month (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
   return firstDayOfMonth.getDay() - 1;
 }
+
+function getDateOutdent(): number {
+  let lastDayOfMonth = getLastDayOfMonth();
+
+  if (lastDayOfMonth.getDay() == 0) {
+    return 0;
+  }
+
+  return 7 - lastDayOfMonth.getDay();
+}
+
 //todo delete
-console.log(calendarData);
+// console.log(calendarData);
 </script>
-  <style>
-  .calendar {
-      font-family: Arial, sans-serif;
-  }
-  
-  .header {
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      margin-bottom: 10px;
-      gap: 15px;
-  }
-  
-  .month-year {
-      font-size: 20px;
-      font-weight: bold;
-      width: 14ch;
-  }
-  
-  .days {
-      display: grid;
-      grid-template-columns: repeat(7, 1fr);
-  }
-  
-  .day {
-      border: 1px solid #ccc;
-      padding: 5px;
-      height: 125px;
-      cursor: pointer;
-  }
-  
-  .selected {
-      border-color: rgb(255, 0, 0);
-  }
-  
-  .current-day {
-      background-color: #ccc;
-  }
-  
-  .day-names {
-      display: grid;
-      grid-template-columns: repeat(7, 1fr);
-      border: 1px solid rgb(110, 110, 110);
-      border-top-left-radius: 5px;
-      border-top-right-radius: 5px;
-  }
-  
-  .day-names div {
-      border-right: 0.5px solid rgb(110, 110, 110);
-      border-left: 0.5px solid rgb(110, 110, 110);
-      font-weight: bold;
-      padding: 10px;
-      font-size: small;
-  }
-  </style>
+<style>
+.calendar {
+    font-family: Arial, sans-serif;
+}
+
+.header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 10px;
+    gap: 15px;
+}
+
+.month-year {
+    font-size: 20px;
+    font-weight: bold;
+    width: 14ch;
+}
+
+.days {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+}
+
+.day {
+    border: 1px solid #ccc;
+    padding: 5px;
+    height: 125px;
+    cursor: pointer;
+}
+
+.past {
+  color: #979797;
+}
+
+.selected {
+    border-color: rgb(255, 0, 0);
+}
+
+.current-day {
+    background-color: #ccc;
+}
+
+.day-names {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    border: 1px solid rgb(110, 110, 110);
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+}
+
+.day-names div {
+    border-right: 0.5px solid rgb(110, 110, 110);
+    border-left: 0.5px solid rgb(110, 110, 110);
+    font-weight: bold;
+    padding: 10px;
+    font-size: small;
+}
+</style>
   
 <div class="calendar">
     <div class="header">
@@ -155,12 +180,19 @@ console.log(calendarData);
 
     <div class="days">
       {#each {length: dateIndent} as _, i}
-        <div class="day" />
+        <div class="day past">
+          {getNextDay(getFirstDayOfMonth(), i - dateIndent).getDate()}
+        </div>
       {/each}
       {#each daysInMonth as day}
         <div class="day">
           {day} <br /> <br />
           {filterDataByDate(new Date(currentDate.getFullYear(), currentDate.getMonth(), day))}
+        </div>
+      {/each}
+      {#each {length: dateOutdent} as _, i}
+        <div class="day past">
+          {getNextDay(getLastDayOfMonth(), i + 1).getDate()}
         </div>
       {/each}
     </div>
