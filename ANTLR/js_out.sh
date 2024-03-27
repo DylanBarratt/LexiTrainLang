f05cb3aeda06eecfe875762a283ace0e2f6f6d0c
DylanBarratt <dylanbarratt03@gmail.com>
2024-03-27 09:27:00 +0000

    added visitor to output files

--- ANTLR/js_out.sh
+++ ANTLR/js_out.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./PeriodFile.g4 -o ../WEB/src/lib/lt -Dlanguage=JavaScript 
+java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./PeriodFile.g4 -o ../WEB/src/lib/lt -Dlanguage=JavaScript -visitor
 
-java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./SessionFile.g4 -o ../WEB/src/lib/lt -Dlanguage=JavaScript 
+java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./SessionFile.g4 -o ../WEB/src/lib/lt -Dlanguage=JavaScript -visitor
 
 echo "JS files generated"
--- ANTLR/out.sh
+++ ANTLR/out.sh
@@ -3,7 +3,7 @@ TYPE="PeriodFile"
 
 rm -rf ./Java
 mkdir ./Java
-java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./$TYPE.g4 -o ./Java
+java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./$TYPE.g4 -o ./Java -visitor
 ls ./Java/*.java
 echo "Java files generated"
 javac ./Java/$TYPE*.java
