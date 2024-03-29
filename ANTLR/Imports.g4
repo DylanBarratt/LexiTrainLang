757f2f409283b4b3d1389bcb3dca81eca1e86af2
DylanBarratt <dylanbarratt03@gmail.com>
2024-03-29 15:35:12 +0000

    added . to import rules

--- ANTLR/Imports.g4
+++ ANTLR/Imports.g4
@@ -4,7 +4,7 @@ file: (importStatement)* EOF;
 
 importStatement: IMPORT;
 
-IMPORT: 'import ' ([a-zA-Z0-9_/]+ | '"' (ESC|.)*? '"');
+IMPORT: 'import ' ([a-zA-Z0-9_/]+ | '"' (ESC|.)*? '"') '.';
 fragment ESC : '\\"' | '\\\\' ;
 
 OTHER: . -> skip;
--- ANTLR/js_out.sh
+++ ANTLR/js_out.sh
@@ -6,4 +6,6 @@ java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./PeriodFile.g4 -o ../WEB/src
 
 java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./SessionFile.g4 -o ../WEB/src/lib/lt -Dlanguage=JavaScript -visitor
 
+java -jar /usr/local/lib/antlr-4.13.1-complete.jar ./Imports.g4 -o ../WEB/src/lib/lt -Dlanguage=JavaScript -visitor
+
 echo "JS files generated"
